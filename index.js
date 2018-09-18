b61f5e168ff52817ba1945b0376536aaa4b70a9c
Maciej Pędzich <maciejp0lepedzich@gmail.com>
2018-09-18 16:44:08 +0200

    showMistakes and bestSuggestion functions added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const pkg_info = require("./package.json"),
       request  = require("request"),
-      export_obj = {};
+	  export_obj = {};
 	  	 
 export_obj.check = function(obj, callback){
 	let url = encodeURI("https://languagetool.org/api/v2/check?language=" + obj.language + "&text=" + obj.text);
@@ -13,19 +13,38 @@ export_obj.check = function(obj, callback){
 			   url = url + rule
 			}
 		  })
-		  request(url, function(error, response, body){
-		    var output = JSON.parse(body);
-		    callback(error, output);
-	      })
+		 sendRequest();
 	 } else if(obj.disabledRules == undefined){
-	     request(url, function(error, response, body){
-		   var output = JSON.parse(body);
-		   callback(error, output);
-		 })
+         sendRequest();
 	 } else{
-		 console.log("Error: disabledRules is not an array!");
-		 return
-	 }
+		 console.log("languagetool-api error: disabledRules is not an array!");
+		 return "Checking aborted!"
+	 } 
+    function sendRequest(){
+	  request(url, function(error, response, body){
+		var output = JSON.parse(body);
+		callback(error, output);
+	  });
+	}
+}
+
+export_obj.showMistakes = function(obj, callback){
+	var mistakes = [];
+	obj.matches.forEach(function(mistake){
+		mistakes.push(mistake.context.text.substr(mistake.context.offset, mistake.context.length));
+	});
+	callback(mistakes);
+}
+
+export_obj.bestSuggestion = function(obj, callback){
+	var suggestions = [];
+    obj.matches.forEach(function(match){
+		suggestions.push({
+			mistake: match.context.text.substr(match.context.offset, match.context.length), 
+			bestSuggestion: match.replacements[0].value
+		});
+	});
+	callback(suggestions);
 }
 
 export_obj.codes = function(){
@@ -40,6 +59,7 @@ export_obj.codes = function(){
 export_obj.info = function(){
 	console.log(pkg_info.name + " by: " + pkg_info.author);
 	console.log("Current version: " + pkg_info.version);
+	console.log("Source code: " + pkg_info.repository.url);
 }
 
 module.exports = export_obj
